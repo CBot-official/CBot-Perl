bcd26b1e5b68fda4a0e41ee2049de34a47ae334f
ComputerTech <60820431+ComputerTech312@users.noreply.github.com>
2021-10-22 02:04:04 +0100

    update config.pl

--- config.pl
+++ config.pl
@@ -1,1 +1,51 @@
-1
+#################################################
+#    _____ ____        _   
+#   / ____|  _ \      | |                  
+#  | |    | |_) | ___ | |_ 
+#  | |    |  _ < / _ \| __|
+#  | |____| |_) | (_) | |_ 
+#   \_____|____/ \___/ \__|
+#
+#################################################
+#
+# Perl release. v0.1
+#################################################
+
+### Start of configuration ###
+
+###
+# Bot's nick.
+##
+$BotNick = "CBot-Perl"
+
+###
+# Bot's alter nick.
+##
+$BotAlt = "CBot-Perl-"
+
+###
+# Bot's ident.
+##
+$BotIdent = "CBot-Perl"
+
+###
+# Bot realname.
+##
+$BotRealname = "CBot-Perl"
+
+### 
+# Bot server.
+##
+$BotServer = "irc.address.org"
+
+###
+# Bot port.
+##
+$BotPort = 6667
+
+###
+# Bot channel.
+##
+$BotHome = "#CBot"
+
+### End of configuration ###
